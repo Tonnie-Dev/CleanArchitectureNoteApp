6456dc770db7dcfaadd77df022c3ec6045d4a6fa
Antony <vontonnie@gmail.com>
2021-11-14 22:33:10 +0300

    Add NoteRepositoryImpl

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl: NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+}
