85a13c5184e4afa1a8b77cffe1a03c23d09adeb7
Antony <vontonnie@gmail.com>
2021-11-14 20:06:32 +0300

    Write getNotes Query

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDAO.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDAO.kt
@@ -1,8 +1,15 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface NoteDAO {
+
+
+    @Query("SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
 }
