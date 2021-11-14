de7802268abd27d1d1cb4137a6af7211019dc8f0
Antony <vontonnie@gmail.com>
2021-11-14 22:37:53 +0300

    Implement method for NoteRepositoryImpl

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -1,23 +1,25 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository
 
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDAO
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
 import kotlinx.coroutines.flow.Flow
 
-class NoteRepositoryImpl: NoteRepository {
+class NoteRepositoryImpl(private val dao: NoteDAO): NoteRepository {
     override fun getNotes(): Flow<List<Note>> {
-        TODO("Not yet implemented")
+
+        return dao.getNotes()
     }
 
     override suspend fun getNoteById(id: Int): Note? {
-        TODO("Not yet implemented")
+      return dao.getNoteById(id)
     }
 
     override suspend fun insertNote(note: Note) {
-        TODO("Not yet implemented")
+        dao.insertNote(note)
     }
 
     override suspend fun deleteNote(note: Note) {
-        TODO("Not yet implemented")
+        dao.deleteNote(note = note)
     }
 }
