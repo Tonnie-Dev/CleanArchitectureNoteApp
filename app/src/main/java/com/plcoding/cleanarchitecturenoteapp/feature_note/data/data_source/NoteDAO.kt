f63ff2186744e100cd678b4b321265ffdff492e2
Antony <vontonnie@gmail.com>
2021-11-14 20:15:17 +0300

    Write getNoteById() Query

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDAO.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDAO.kt
@@ -9,7 +9,12 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface NoteDAO {
 
+// TODO: 14-Nov-21 Confirm table Name
 
+    /*This is not a suspend fxn since it is wrapped with Flow from Coroutines*/
     @Query("SELECT * FROM note")
     fun getNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note WHERE id =:id")
+    suspend fun getNoteById(id:Int):Note?
 }
