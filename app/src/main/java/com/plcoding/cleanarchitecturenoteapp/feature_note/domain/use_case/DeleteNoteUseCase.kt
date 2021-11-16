443a620364a26b7f0394ccf379d40881508c81b3
Antony <vontonnie@gmail.com>
2021-11-16 08:04:30 +0300

    Complete DeleteNoteUseCase

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNoteUseCase.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNoteUseCase.kt
@@ -1,4 +1,12 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
-class DeleteNoteUseCase {
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNoteUseCase (private val repo:NoteRepository){
+
+    suspend operator fun invoke(note: Note){
+
+        repo.deleteNote(note)
+    }
 }
