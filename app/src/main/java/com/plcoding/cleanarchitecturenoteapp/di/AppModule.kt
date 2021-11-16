e1f301be8251a55c6265d25854e3e47cd4d9c009
Antony <vontonnie@gmail.com>
2021-11-16 08:35:37 +0300

    write provideRoomDatabase() fxn

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
@@ -10,15 +10,31 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
 @Module
+
+//scope this to the application lifecycle to live throughout the app life
 @InstallIn(SingletonComponent::class)
+
+// make the module an object which cannot be instantiated
 object AppModule {
 
+
+    //ROOM
     @Provides
     @Singleton
-    fun providesRoomDatabase(@ApplicationContext context: Context){
-        val room = Room.databaseBuilder(context,NoteDatabase::class.java,"Notes")
+
+    //method returns the abstract data base class
+
+    //the method takes a context to create the RoomDatabase
+    fun provideRoomDatabase(@ApplicationContext context: Context): NoteDatabase {
+
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            NoteDatabase.DB_NAME
+        )
+            .build()
 
     }
 }
+
