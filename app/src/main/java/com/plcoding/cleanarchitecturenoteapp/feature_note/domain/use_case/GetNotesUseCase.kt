ba6ad46e3ce161057f7bc2af22a2ea2d2c5cf584
Antony <vontonnie@gmail.com>
2021-11-16 06:57:20 +0300

    Write the Invoke Fxn shell

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesUseCase.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesUseCase.kt
@@ -1,7 +1,12 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
-class GetNotesUseCase {
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetNotesUseCase (private val repository: NoteRepository){
 
 
+    operator fun invoke():Flow<List<Note>>{}
 
 }
---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/util/NoteOrder.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util
+
+sealed class NoteOrder(val orderType: OrderType){
+
+
+    class Title(orderType: OrderType):NoteOrder(orderType = orderType)
+    class Color(orderType: OrderType):NoteOrder(orderType = orderType)
+    class Date(orderType: OrderType):NoteOrder(orderType = orderType)
+
+
+
+}
