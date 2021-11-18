d2670a71615cee90c2d91cf27e1c566d0a40c5c4
Antony <vontonnie@gmail.com>
2021-11-18 20:19:52 +0300

    Add the input validation logic for addNote() use case

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/AddNoteUseCase.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/AddNoteUseCase.kt
@@ -1,12 +1,27 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.InvalidNoteException
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
 
-class AddNoteUseCase (val repository: NoteRepository){
+class AddNoteUseCase (private val repository: NoteRepository){
 
+//make invoke fxn throw an exemption
 
+    @Throws (InvalidNoteException::class)
     suspend operator fun invoke(note:Note){
+
+        //logic for note validation
+        if (note.title.isBlank()){
+
+            throw InvalidNoteException(message = "The title of the note cannot be empty")
+        }
+        if (note.content.isBlank()){
+
+
+            throw InvalidNoteException(message = "The content of the note cannot be empty")
+        }
+
 
         repository.insertNote(note)
     }
