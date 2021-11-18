cdbd4b7c68cd6ac891994057c5115a6feb125b73
Antony <vontonnie@gmail.com>
2021-11-18 19:09:45 +0300

    Create NotesState class

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesState.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+
+data class NotesState(
+    val notes: List<Note> = emptyList(),
+    val noteOrder: NoteOrder = NoteOrder.Date(orderType = OrderType.Descending),
+    val isOrderSelectionVisible: Boolean = false
+)
+
--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,6 +1,9 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.lifecycle.ViewModel
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class @Inject constructor(): ViewModel{}
+class NotesViewModel @Inject constructor(notesUseCase: GetNotesUseCase): ViewModel() {}
