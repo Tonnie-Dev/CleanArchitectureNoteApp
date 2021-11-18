bd4d016f615bb2facf791420db55f2f74ac22f95
Antony <vontonnie@gmail.com>
2021-11-18 19:29:40 +0300

    Add onEvent() inside the ViewModel

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,9 +1,31 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class NotesViewModel @Inject constructor(notesUseCase: GetNotesUseCase): ViewModel() {}
+class NotesViewModel @Inject constructor(notesUseCase: GetNotesUseCase): ViewModel() {
+
+    //state comes down & will be observed by the UI
+
+    var state = mutableStateOf(NotesState())
+    private set
+
+
+//events from UI
+    fun onEvent(event: NotesEvent){
+
+    //events from the UI - Events go up
+        when(event){
+
+
+            is NotesEvent.Order ->{}
+            is NotesEvent.DeleteNote ->{}
+            is NotesEvent.RestoreNote ->{}
+            is NotesEvent.ToggleOrderSelection ->{}
+        }
+    }
+}
