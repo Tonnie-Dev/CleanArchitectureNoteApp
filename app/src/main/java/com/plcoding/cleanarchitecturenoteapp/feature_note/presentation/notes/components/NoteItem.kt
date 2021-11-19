20ec1cb252ceab8c93e884cad424a0dd6fb286cf
Antony <vontonnie@gmail.com>
2021-11-19 20:31:52 +0300

    Draw Path

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/NoteItem.kt
@@ -1,7 +1,11 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
@@ -16,4 +20,22 @@ fun NoteItem(
     onDelete: () -> Unit
 ) {
 
+
+    Box(modifier = modifier){
+
+Canvas(modifier = Modifier.matchParentSize()){
+
+    val clipPath = Path().apply {
+
+        lineTo(size.width - cutCornerSize.toPx(), 0f)
+        lineTo(size.width, cutCornerSize.toPx())
+        lineTo(size.width, size.height )
+        lineTo(0f, size.height)
+        close()
+    }
+
+
+}
+
+    }
 }
