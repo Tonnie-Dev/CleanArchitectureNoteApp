02d11997a363a001a81a74b1d6f0b38eba58b322
Antony <vontonnie@gmail.com>
2021-11-19 21:04:04 +0300

    Add Note Title Text

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/NoteItem.kt
@@ -3,6 +3,11 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.co
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
@@ -11,6 +16,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.ColorUtils
@@ -27,38 +33,54 @@ fun NoteItem(
 ) {
 
 
-    Box(modifier = modifier){
+    Box(modifier = modifier) {
 
-Canvas(modifier = Modifier.matchParentSize()){
+        Canvas(modifier = Modifier.matchParentSize()) {
 
-    val clipPath = Path().apply {
+            val clipPath = Path().apply {
 
-        lineTo(size.width - cutCornerSize.toPx(), 0f)
-        lineTo(size.width, cutCornerSize.toPx())
-        lineTo(size.width, size.height )
-        lineTo(0f, size.height)
-        close()
-    }
+                lineTo(size.width - cutCornerSize.toPx(), 0f)
+                lineTo(size.width, cutCornerSize.toPx())
+                lineTo(size.width, size.height)
+                lineTo(0f, size.height)
+                close()
+            }
 
 
-    clipPath(path = clipPath){
+            clipPath(path = clipPath) {
 
-        drawRoundRect(
-            size = size,
-            color = Color(note.color),
-            cornerRadius = CornerRadius(cornerRadius.toPx())
-        )
+                drawRoundRect(
+                    size = size,
+                    color = Color(note.color),
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
+                )
 
-        drawRoundRect(
-            size = Size(cutCornerSize.toPx() + 100f, cutCornerSize.toPx() + 100f),
+                drawRoundRect(
+                    size = Size(cutCornerSize.toPx() + 100f, cutCornerSize.toPx() + 100f),
 
-            color = Color(ColorUtils.blendARGB(note.color, 0x000000,0.2f)),
-            cornerRadius = CornerRadius(cornerRadius.toPx()),
-            topLeft = Offset(size.width - cutCornerSize.toPx(), -100f)
-        )
-    }
+                    color = Color(ColorUtils.blendARGB(note.color, 0x000000, 0.2f)),
+                    cornerRadius = CornerRadius(cornerRadius.toPx()),
+                    topLeft = Offset(size.width - cutCornerSize.toPx(), -100f)
+                )
+            }
 
-}
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(end = 32.dp)
+        ) {
+
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.h6,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
 
     }
 }
