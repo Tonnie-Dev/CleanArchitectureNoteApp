02420d05ed39145d368c55bcd8c040be2c6423f2
Antony <vontonnie@gmail.com>
2021-11-21 10:38:13 +0300

    Add FloatingActionButton

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -1,7 +1,11 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavController
 
@@ -13,5 +17,16 @@ fun NotesScreen(navController: NavController, viewModel: NotesViewModel) {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
 
+
+    //Scaffold
+
+    Scaffold() {
+        FloatingActionButton(
+            onClick = { /*TODO*/ },
+            backgroundColor = MaterialTheme.colors.primary
+        ) {
+            
+        }
+    }
 
 }
