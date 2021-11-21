c77e41ebb69b785f650051180e5b824477cf9c9a
Antony <vontonnie@gmail.com>
2021-11-21 10:33:38 +0300

    Declare variables for the NotesViewModel

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -1,8 +1,17 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavController
 
 @Composable
-fun NotesScreen() {
-    
+fun NotesScreen(navController: NavController, viewModel: NotesViewModel) {
+
+//variables
+    val state by viewModel.state
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+
 }
