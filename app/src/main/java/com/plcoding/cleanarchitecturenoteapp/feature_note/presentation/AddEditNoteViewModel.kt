f720eb54020101c2e2560c913ec2c9303d292351
Antony <vontonnie@gmail.com>
2021-11-22 14:41:27 +0300

    Add NoteTextFieldState data class

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/AddEditNoteViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/AddEditNoteViewModel.kt
@@ -2,10 +2,11 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation
 
 import androidx.lifecycle.ViewModel
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNoteByIdUseCase
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NotesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class AddEditNoteViewModel @Inject constructor(private val useCase:GetNoteByIdUseCase) :
+class AddEditNoteViewModel @Inject constructor(private val useCase:NotesUseCase) :
     ViewModel() {
 }
