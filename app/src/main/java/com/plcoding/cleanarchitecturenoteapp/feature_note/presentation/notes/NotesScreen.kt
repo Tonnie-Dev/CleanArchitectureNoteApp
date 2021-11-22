809dd2328a81c335026575e68cc4d34dafc5cb92
Antony <vontonnie@gmail.com>
2021-11-22 11:12:45 +0300

    Add Text Header

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -1,11 +1,14 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
@@ -18,15 +21,33 @@ fun NotesScreen(navController: NavController, viewModel: NotesViewModel) {
 
 
     //Scaffold
-
-    Scaffold() {
+    Scaffold(floatingActionButton = {
         FloatingActionButton(
             onClick = { /*TODO*/ },
             backgroundColor = MaterialTheme.colors.primary
         ) {
             //Content of FAB
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add note"
+            )
+        }
+    }) {
 
-            Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")
+        Column(modifier = Modifier
+
+            .fillMaxSize()
+            .padding(it)) {
+            
+            
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(text = "Your Notes", style = MaterialTheme.typography.h4)
+            }
         }
     }
 
