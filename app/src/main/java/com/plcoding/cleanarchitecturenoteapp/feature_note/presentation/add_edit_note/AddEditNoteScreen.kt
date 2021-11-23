0fc6aaa09e8b02b1b0f23a48e9c7266f1cb827aa
Antony <vontonnie@gmail.com>
2021-11-23 11:51:37 +0300

    Add AddEditNoteScreen composable

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
@@ -0,0 +1,11 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AddEditNoteScreen(navController: NavController,
+noteColor:Int,
+viewModel: AddEditNoteViewModel = hiltViewModel()) {
+
+}
