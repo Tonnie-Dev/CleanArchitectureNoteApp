7e870c8a124ad1b851c2ef45047e627c2f7f0204
Antony <vontonnie@gmail.com>
2021-11-23 11:48:10 +0300

    Add basictextfield to the TransparentHintTextField

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
@@ -1,8 +1,14 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 
 
@@ -12,11 +18,32 @@ fun TransparentHintTextField(
     hint: String,
     modifier: Modifier = Modifier,
     isHintVisible: Boolean = true,
-    onValueChanged: (String) -> Unit,
+    onValueChange: (String) -> Unit,
     textStyle: TextStyle = TextStyle(),
     singleLine: Boolean = false,
-    onFocusChanged: (FocusState) -> Unit
+    onFocusChange: (FocusState) -> Unit
 
 ) {
 
+    // TODO: 23-Nov-21 test the up & below modifier
+    Box(modifier = modifier ){
+        BasicTextField(
+            value = text,
+            onValueChange = onValueChange,
+            singleLine = singleLine,
+            textStyle = textStyle,
+            modifier = modifier
+                .fillMaxWidth()
+                .onFocusChanged {
+
+                    onFocusChange(it)
+                }
+        )
+        if (isHintVisible){
+
+            Text(text = hint, style = textStyle, color = Color.DarkGray)
+
+        }
+
+    }
 }
