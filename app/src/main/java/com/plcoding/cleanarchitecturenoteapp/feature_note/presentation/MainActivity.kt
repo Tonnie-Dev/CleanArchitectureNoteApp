8aef9bcf0f9dbb39779f58342bc9ab3295469cdb
Antony <vontonnie@gmail.com>
2021-11-24 08:31:52 +0300

    Add NavHost

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
@@ -3,18 +3,41 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.NotesScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screens
 import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CleanArchitectureNoteAppTheme {
+
+                Surface() {
+
+
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = Screens.NotesScreen.route){
+
+                        composable(Screens.NotesScreen.route){
+
+                            NotesScreen(navController = navController)
+                        }
+
+                    }
+                }
                 
             }
         }
