63a3e84c70bd8dde9ff33da3eeaa95e99ab18b62
Antony <vontonnie@gmail.com>
2021-11-24 20:09:33 +0300

    Add Nav composables

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
@@ -9,14 +9,18 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
 import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.NotesScreen
 import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screens
 import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+import dagger.hilt.EntryPoint
 
+@EntryPoint // to inject viewModel with Dagger Hilt into this activity
 class MainActivity : ComponentActivity() {
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CleanArchitectureNoteAppTheme {
 
-                Surface() {
+                Surface(color = MaterialTheme.colors.background) {
 
 
                     val navController = rememberNavController()
@@ -34,6 +38,24 @@ class MainActivity : ComponentActivity() {
                         composable(Screens.NotesScreen.route){
 
                             NotesScreen(navController = navController)
+                        }
+
+                        composable(route = Screens.AddEditNoteScreen.route +
+
+                        "?noteId={noteId}&noteColor={noteColor}",
+                        arguments = listOf(navArgument(name = "noteId"){
+                            type = NavType.IntType
+                            defaultValue = -1
+                        }, navArgument(name = "noteColor"){
+
+                            type = NavType.IntType
+                            defaultValue = -1
+                        })
+                            ){
+
+                            val color = it.arguments?.getInt("noteColor")?: -1
+
+                            AddEditNoteScreen(navController = navController, noteColor = color)
                         }
 
                     }
