694f0a9ce9d3e23cc6bcd5ce8a3426d32d8e1e12
Antony <vontonnie@gmail.com>
2021-11-27 07:41:31 +0300

    Initialize Timber on Application's onCreate()

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/NoteApp.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/NoteApp.kt
@@ -2,7 +2,13 @@ package com.plcoding.cleanarchitecturenoteapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class NoteApp:Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
